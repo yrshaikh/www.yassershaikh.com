c9015f8bd0b911df1cf899ca2e9cdf2aaf9c5956
Yasser Shaikh <yasser.shaikh@agoda.com>
2019-08-13 23:47:13 +0700

    Responsive.

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -8,7 +8,7 @@ class Home extends React.Component {
     return (
       <div className="Home">
         <Row>
-          <Col span={6}>
+          <Col span={6} xs={24}>
             <div className="Home__Shouts">
               <div className="Home__Shout1">Hi there! My name is</div>
               <img
@@ -28,7 +28,7 @@ class Home extends React.Component {
               </div>
             </div>
           </Col>
-          <Col span={18}>
+          <Col span={18} xs={0}>
             <ResumePage />
           </Col>
         </Row>
