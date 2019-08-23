f75953c40740c83b756e08900b177f0e61c72b55
Yasser Shaikh <yasser.shaikh@agoda.com>
2019-08-24 00:05:24 +0700

    Added menu bar on desktop site.

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Col, Row } from "antd";
+import {Col, Icon, Menu, Row} from "antd";
 import React from "react";
 import ResumePage from "../Resume/ResumePage";
 import TitleAndSubTitle from "./Components/TitleAndSubTitle/TitleAndSubTitle";
@@ -11,6 +11,18 @@ class Home extends React.Component<IProps, {}> {
   public render(): JSX.Element {
     return (
       <div className="Home">
+        <Menu mode="horizontal" theme={"dark"}>
+          <Menu.Item key="home">
+            <Icon type="home" />
+            Home
+          </Menu.Item>
+          <Menu.Item key="medium">
+            <a href="https://medium.com/yasser-shaikh" target="_blank" rel="noopener noreferrer">
+              <Icon type="book" />
+              Blog
+            </a>
+          </Menu.Item>
+        </Menu>
         <Row>
           <Col className="Home__Welcome" span={24}>
             <TitleAndSubTitle isMobile={this.props.isMobile} />
